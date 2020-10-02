08508915a5e2002e1a9973224436975b017f069e
tech-me-up <sejaljshah1205@gmail.com>
2020-10-02 18:40:36 +0530

    first

---
+++ img_reading-writing.py
@@ -0,0 +1,6 @@
+import cv2
+img=cv2.imread("E:\\Sample codes\\open cv\\img1-techboy.jpg",1)
+cv2.imshow("image",img)
+k=cv2.waitKey(0)
+cv2.destroyAllWindows()
+cv2.imwrite("E:\\Sample codes\\open cv\\img1-techboy_copy.jpg",img)
